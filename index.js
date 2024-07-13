660c5bf4d25ec0f289ab723e22ea37d6213ae166
Shakil57375 <shakil57375@gmail.com>
2024-07-13 22:59:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lc40fqb.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db("DiverseBlog").collection("users");
+        const blogsCollection = client.db("DiverseBlog").collection("blogs");
+
+        app.post("/users", async (req, res) => {
+            const body = req.body;
+            const query = { email: body.email };
+            const existingUSer = await userCollection.findOne(query);
+            if (existingUSer) {
+                return res.send({ message: "user already exists" });
+            }
+            const result = await userCollection.insertOne(body);
+            res.send(result);
+        });
+
+        app.post("/blog", async (req, res)=>{
+            const newBlog = req.body;
+            const result = await blogsCollection.insertOne(newBlog);
+            res.send(result);
+        })
+
+        app.get("/blogs", async (req, res) => {
+            const cursor = blogsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get("/users", async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get("/blogs/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const result = await blogsCollection.findOne(filter);
+            res.send(result);
+        });
+
+        app.delete("/deleteBlogs/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Diverse blog is running");
+});
+
+app.listen(port, () => {
+    console.log(`Diverse blog is running on port, ${port}`);
+});
